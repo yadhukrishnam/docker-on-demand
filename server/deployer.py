3d50d282fdcb92c7125e686eaa719ec355b09205
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2021-07-31 18:36:34 +0530

    Update server

--- server/deployer.py
+++ server/deployer.py
@@ -1,96 +1,46 @@
 from flask import Flask, request, jsonify, g
 import requests
-import jwt
-import uuid
-import sqlite3
 import subprocess
+import docker
 import json
 
 app = Flask(__name__)
 
-SECRET = "s$cr$t"
-DATABASE = 'deployments.db'
+def build_images(challenges):
+    client = docker.from_env()
+    for challenge in challenges:
+        subprocess.run(["docker-compose build"], cwd=challenges[challenge]["path"], shell=True, check=True)
+    client.close()
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+def deploy(image, local_port, public_port):
+    client = docker.from_env()
+    container = client.containers.run(image, ports={f"{public_port}" : local_port}, detach=True)
+    container_id = container.id
+    client.close()
+    return container_id
 
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+def kill_container(container_id):
+    client = docker.from_env()
+    container = client.containers.get(container_id)
+    container.kill()
+    client.close()
 
-def deploy(challenge_id):
-    #TODO: Implement docker deployment
-    if challenge_id in challenges:
-        # print (challenges[challenge_id])
-        subprocess.Popen(["sudo", "docker", "run -p", ])
-    return str(uuid.uuid4()), "43306"
+@app.route("/request_deploy", methods=["POST"])
+def request_deploy():
+    body = request.get_json()
+    image = body["image"]
+    local_port = body["local_port"]
+    public_port = body["public_port"]
+    container_id  = deploy(image, local_port, public_port)
+    return jsonify({"status": "success", "container_id" : container_id})
 
-def kill(challenge_id):
-    #TODO: Implement docker deletion
-    pass
+@app.route("/request_kill", methods=["POST"])
+def request_kill():
+    body = request.get_json()
+    container_id = body["container_id"]
+    kill_container(container_id)
+    return jsonify({"status": "success"})
 
-@app.route('/deploy', methods=['POST'])
-def deploy_challenge():
-    body = request.get_json()["body"]
-    try:
-        payload = jwt.decode(body, SECRET, algorithms=["HS256"])
-        challenge_id = str(payload["challenge_id"])
-        user_id = str(payload["user_id"])
 
-    except (jwt.DecodeError, jwt.ExpiredSignatureError):
-        return jsonify({'status': 'fail', 'message': 'Token is invalid'})
-        
-    deployment = query_db("SELECT * FROM deployments WHERE user_id = ? AND challenge_id = ?", (user_id, challenge_id) , True)
-
-    if deployment is None:
-        id, port = deploy(challenge_id)
-        conn = sqlite3.connect(DATABASE)
-        conn.execute("INSERT INTO deployments  VALUES (?, ?, ?, ?)", (id, user_id, challenge_id, str(port)))
-        conn.commit()
-        conn.close()
-
-        return jsonify({"status":"success", 'deployment_id': id, "port": port})
-    else:
-        print(deployment)
-        return jsonify({"status":"success", 'deployment_id': deployment[0], "port" : deployment[3]})
-
-    
-@app.route('/kill', methods=['POST'])
-def kill_challenge():
-    body = request.get_json()["body"]
-    try:
-        payload = jwt.decode(body, SECRET, algorithms=["HS256"])
-        challenge_id = str(payload["challenge_id"])
-        user_id = str(payload["user_id"])
-        deployment_id = str(payload["deployment_id"])
-
-    except (jwt.DecodeError, jwt.ExpiredSignatureError):
-        return jsonify({'status': 'fail', 'message': 'Token is invalid'})
-    
-    deployment = query_db("SELECT * FROM deployments WHERE user_id = ? AND challenge_id = ? AND deployment_id = ?", (user_id, challenge_id, deployment_id) , True)
-    
-    if deployment is not None:
-        port = deployment[3]
-        print ("Deleting deployment at", port)
-        conn = sqlite3.connect(DATABASE)
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM deployments WHERE user_id = ? AND challenge_id = ? ", (user_id, challenge_id,))
-        deleted = cursor.rowcount
-        conn.commit()
-        conn.close()
-        return jsonify({"status":"success"})
-    else:
-        return jsonify({"status":"fail"})
-        
-   
 if __name__ == '__main__':
-    challenges = json.load(open("challenges.json"))
-    conn = sqlite3.connect(DATABASE)
-    conn.execute('''CREATE TABLE IF NOT EXISTS deployments  (deployment_id, user_id, challenge_id, port); ''')
-    conn.close()
     app.run(host='0.0.0.0', port=9999, debug=True)
