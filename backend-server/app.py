3e417121633016eb9efa25df14a71b9ef333b862
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2021-08-09 19:22:34 +0530

    Hide warning

--- backend-server/app.py
+++ backend-server/app.py
@@ -14,6 +14,7 @@ from deployer import *
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Deployment(db.Model):
