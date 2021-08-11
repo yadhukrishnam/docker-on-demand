c6ccbff29a910b1813c5c2eedb70ba76e84617f2
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2021-08-11 15:32:24 +0530

    Update requirements

--- server/config.py
+++ server/config.py
@@ -1,10 +1,10 @@
 import json
 
-debug = False
 expiry = 2
 PORT_START = 3000
 PORT_END = 8000
 APP_PORT = 1337
 HOST_IP = "http://34.136.149.64"
-challenges = json.load(open("challenges.json"))
+CLIENT_IP = ""
 SECRET = "s$cr$t"
+challenges = json.load(open("challenges.json"))
