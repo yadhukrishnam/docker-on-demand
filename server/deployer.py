c0e4a763b97a68ba042c36418698aabcf366ae6a
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2021-08-13 12:39:33 +0530

    remove special chars

--- server/deployer.py
+++ server/deployer.py
@@ -3,12 +3,14 @@ import docker
 import json
 import subprocess
 from config import *
+import re
 
 def deploy(challenge_id, public_port, container_name):
     if challenge_id in challenges:
         print ("Deploying,", challenges[challenge_id])
         local_port = challenges[challenge_id]["local_port"]
         client = docker.from_env()
+        container_name = re.sub('[^A-Za-z0-9]+', '_', container_name) + "_" + str(public_port)
         container = client.containers.run(challenge_id, ports={f"{local_port}" : public_port}, detach=True, name=container_name)
         container_id = container.id
         client.close()
