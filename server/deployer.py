e674c723680a6b5c73a072c0c0a920dd1768a5ca
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2022-01-13 12:38:15 +0530

    Format deployer

--- server/deployer.py
+++ server/deployer.py
@@ -2,17 +2,21 @@ import docker
 from config import *
 import re
 
+
 def deploy(image_id, public_port, container_name):
     if image_id in images:
-        print ("Deploying,", images[image_id])
+        print("Deploying,", images[image_id])
         local_port = images[image_id]["local_port"]
         client = docker.from_env()
-        container_name = re.sub('[^A-Za-z0-9]+', '_', container_name) + "_" + str(public_port)
-        container = client.containers.run(image_id, ports={f"{local_port}" : public_port}, detach=True, name=container_name)
+        container_name = re.sub('[^A-Za-z0-9]+', '_',
+                                container_name) + "_" + str(public_port)
+        container = client.containers.run(
+            image_id, ports={f"{local_port}": public_port}, detach=True, name=container_name)
         container_id = container.id
         client.close()
         return container_id
     return None
+
 
 def kill(container_id):
     try:
