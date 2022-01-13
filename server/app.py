de9f62912fa5a429d0b773f9723afc2e4b91bbc5
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2022-01-13 12:28:42 +0530

    Add UI support

--- server/app.py
+++ server/app.py
@@ -1,21 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
+from flask.templating import render_template
+from flask_httpauth import HTTPBasicAuth
+from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+from sqlalchemy import text
 import threading
 import os
 import logging
 import random
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
 import time
+import base64
 from deployer import *
-from config import SECRET, images
+from config import credentials, images
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'database.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-logging.basicConfig(filename='debug.log', level=logging.WARNING, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logging.basicConfig(filename='debug.log', level=logging.WARNING,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
 
 class Deployment(db.Model):
     deployment_id = db.Column(db.String(65), primary_key=True)
@@ -23,7 +31,7 @@ class Deployment(db.Model):
     image_id = db.Column(db.String(200), nullable=False)
     port = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.String(80), nullable=False)
-    
+
     def __init__(self, deployment_id, user_id, image_id, port, created_at):
         self.deployment_id = deployment_id
         self.user_id = user_id
@@ -31,58 +39,100 @@ class Deployment(db.Model):
         self.port = port
         self.created_at = created_at
 
+
 def secure(params=None):
     def decorator(f):
         @wraps(f)
         def check_authorization(*args, **kwargs):
-            if request.headers.get("X-Auth") == SECRET:
-                try:
-                    body = request.get_json()
-                    if params != None:
-                        missing = [r for r in params if r not in body]
-                        if missing:
-                            raise ValueError("Required params not supplied.")
-                except:
-                    response = {'status': 'fail', 'message': 'Invalid JSON body.'}
-                    return jsonify(response), 400
+            try:
+                body = request.get_json()
 
-                if params is None:
-                    return f()
-                else:
-                    return f(* tuple(body[item] for item in params))
-            else:
-                response = {'status': 'fail', 'message': 'Invalid token supplied.'}
+                if params != None:
+                    missing = [r for r in params if r not in body]
+                    if missing:
+                        raise ValueError("Required params not supplied.")
+            except:
+                response = {'status': 'fail', 'message': 'Invalid JSON body.'}
                 return jsonify(response), 400
 
+            if params is None:
+                return f()
+            else:
+                return f(* tuple(body[item] for item in params))
         return check_authorization
     return decorator
 
 
+def is_admin():
+    def decorator(f):
+        @wraps(f)
+        def check_admin(*args, **kwards):
+            return auth.current_user() == "admin"
+        return check_admin
+    return decorator
+
+
 def auto_clear():
-    sql = text('Select * FROM deployment WHERE :time - created_at > 60').bindparams(time=time.time())
+    sql = text(
+        'Select * FROM deployment WHERE :time - created_at > 60').bindparams(time=time.time())
     expired_deployments = db.session.execute(sql).all()
     for deployment in expired_deployments:
         remove_deployment(deployment[0])
     db.session.commit()
     threading.Timer(20, auto_clear).start()
 
+
 def remove_deployment(deployment_id):
     kill(deployment_id)
     Deployment.query.filter_by(deployment_id=deployment_id).delete()
     db.session.commit()
 
+
 @app.errorhandler(Exception)
 def server_error(err):
     app.logger.exception(err)
-    return jsonify({'status': 'fail'})  
+    return jsonify({'status': 'fail'})
 
-@app.route('/api/get_deployments', methods=['POST'])
-@secure()
-def get_deployments():
-    result = [image for image in images]
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in credentials and \
+            (credentials.get(username) == password):
+        return username
+
+
+@app.route('/api/get_images', methods=['GET'])
+@auth.login_required
+def get_images():
+    result = []
+    for image in images:
+        result.append(
+            {"imagename": image, "port": images[image]["local_port"]})
+
     return jsonify({'status': 'success', "images": result})
 
-@app.route('/api/get_active_deployments', methods=['POST'])
+
+@app.route('/api/get_deployments', methods=['POST'])
+@auth.login_required
+def get_deployments():
+    deployments = Deployment.query.filter_by().all()
+    if deployments is None:
+        return jsonify({'status': 'fail', 'message': 'No deployments found.'}), 404
+    else:
+        result = {}
+        for deployment in deployments:
+            print(deployment.user_id)
+            result[deployment.image_id] = {
+                "port": deployment.port,
+                "user_id": deployment.user_id,
+                "deployment_id": deployment.deployment_id,
+                "created_at": deployment.created_at
+            }
+        return jsonify({'status': 'success', 'deployments': result})
+
+
+@app.route('/api/get_user_deployments', methods=['POST'])
+@auth.login_required
 @secure(["user_id"])
 def get_active_deployments(user_id):
     deployments = Deployment.query.filter_by(user_id=user_id).all()
@@ -95,11 +145,14 @@ def get_active_deployments(user_id):
                 "url": f"{HOST_IP}:{deployment.port}/"
             }
         return jsonify({'status': 'success', 'deployments': result})
-    
+
+
 @app.route('/api/deploy', methods=['POST'])
+@auth.login_required
 @secure(["image_id", "user_id"])
-def deploy_image(image_id, user_id):   
-    deployment = Deployment.query.filter_by(user_id=user_id, image_id=image_id).first()
+def deploy_image(image_id, user_id):
+    deployment = Deployment.query.filter_by(
+        user_id=user_id, image_id=image_id).first()
     if deployment is None:
         while True:
             port = random.randint(PORT_START, PORT_END)
@@ -111,24 +164,51 @@ def deploy_image(image_id, user_id):
         deployment = Deployment(id, user_id, image_id, port, time.time())
         db.session.add(deployment)
         db.session.commit()
-        return jsonify({"status":"success", "url": f"{HOST_IP}:{port}/"})
+        return jsonify({"status": "success", "url": f"{HOST_IP}:{port}/"})
     else:
         print(deployment)
-        return jsonify({"status":"success", "url" : f"{HOST_IP}:{deployment.port}/"})
+        return jsonify({"status": "success", "url": f"{HOST_IP}:{deployment.port}/"})
 
-    
+
 @app.route('/api/kill', methods=['POST'])
+@auth.login_required
 @secure(["image_id", "user_id"])
 def kill_image(image_id, user_id):
-    deployment = Deployment.query.filter_by(user_id=user_id, image_id=image_id).first()
+    deployment = Deployment.query.filter_by(
+        user_id=user_id, image_id=image_id).first()
     if deployment is not None:
         remove_deployment(deployment.deployment_id)
-        return jsonify({"status":"success"})
+        return jsonify({"status": "success"})
     else:
-        return jsonify({'status':'fail', 'message': 'No such deployment.'}), 404
+        return jsonify({'status': 'fail', 'message': 'No such deployment.'}), 404
+
+
+@app.route("/")
+@auth.login_required
+def index():
+    return redirect("/dashboard")
+
+
+@app.route("/dashboard")
+@auth.login_required
+def dashboard():
+    return render_template("index.html")
+
+
+@app.route("/deployments")
+@auth.login_required
+def deployments():
+    return render_template("deployments.html")
+
+
+@app.route("/logout")
+@auth.login_required
+def logout():
+    return "Bye!", 401
+
 
 db.create_all()
 auto_clear()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=APP_PORT, debug=True) 
+    app.run(host='0.0.0.0', port=APP_PORT, debug=True)
--- server/config.py
+++ server/config.py
@@ -4,5 +4,10 @@ PORT_START = 40000
 PORT_END = 60000
 APP_PORT = 5015
 HOST_IP = "http://host"
-SECRET = "<redacted>"
+
+credentials = {
+    "admin": "admin",
+    "user" : "user"
+}
+
 images = json.load(open("./config/images.json"))
