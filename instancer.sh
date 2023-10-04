2e6955300ab3c253a91bf2576db40c09424278f8
Yadhu Krishna <hello@yadhu.in>
2023-10-04 16:00:43 +0530

    move instancer to different branch

--- instancer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ./instancer
-python3 ./instancer/app.py > log.txt 2>&1 &
-echo "Frontend spawned"
-cd ./backend/server
-python3 ./backend/server/app.py > log.txt  2>&1 &
-echo "Backend spawned"
-# script to run both docker-on-demand and instancer
